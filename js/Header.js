116bee49cf06909f8c9f2e772fff30e25becd69b
Temidayo Olawoyin <t.olawoyin@outlook.com>
2022-07-09 06:16:18 +0100

    A little modification

--- js/Header.js
+++ js/Header.js
@@ -1,6 +1,6 @@
 const HEADER = (component, props) => {
     component.innerHTML += `<header class="pt-5">
-        <a href="https://olawoyin.netlify.app/"><img
+        <a href="https://www.codewars.com/users/Olawoyin"><img
                 class="d-block m-auto"
                 src="https://www.codewars.com/users/Olawoyin/badges/large"
                 alt=""/>
